d84016a38d3a204baa3bfa72ede882c57b4d430e
naman-soni09 <naman.soni.cg@gmail.com>
2026-08-23 20:36:33 +0530

    added all files and folder

---
+++ python variables/variables.py
@@ -0,0 +1,44 @@
+# question 24
+name="Naman"
+age=18
+city="Kalol"
+print(name,age,city)
+
+# question 25
+student_name = "Rahul"
+student_roll_number = 101
+student_branch = "Computer Science"
+print(student_name,student_roll_number,student_branch)
+
+# question 26
+marks = 75
+print(marks)
+marks = 90
+print(marks) 
+marks = 75
+marks = 90
+print(marks)
+
+# question 27
+name, age, city = "Rahul", 18, "Patna"
+print(name)
+print(age)
+print(city)
+
+# question 28
+x = y = z = 0
+print(x, y, z)
+
+# question 29
+student_name = "Rahul"
+student_full_name = "Rahul"
+student_class = "B.Tech"
+print(student_name,student_full_name,student_class)
+
+# question 30
+# student's information.
+student_name = "Rahul"
+student_age = 18
+student_age = 19
+print("Student name:", student_name)
+print("Student age:", student_age)
