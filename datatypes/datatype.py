0a4332d041976066aedfc9deb8402edaac2ee9b3
naman-soni09 <naman.soni.cg@gmail.com>
2026-08-24 18:12:05 +0530

    datatypes added

---
+++ datatypes/datatype.py
@@ -0,0 +1,54 @@
+# question 24
+
+student_name = "naman"
+student_age = 19
+student_height = 5.9
+whether_the_person_is_a_student = True
+student_result = None
+print(type(student_name))
+print(type(student_age))
+print(type(student_height))
+print(type(whether_the_person_is_a_student))
+print(type(student_result))
+
+#question 25
+
+a = 50
+b = 50.0
+c = "50"
+print(type(a),type(b),type(c))
+
+#question 27
+
+a = None
+b = "None"
+print(type(a),type(b))
+
+#question 28
+
+value = 90
+print(type(value))
+
+value = "90"
+print(type(value))
+#Explaination- both value is same that is 90 but the data type is different in first there is no commas beacuse it is treated as integer but in second there is double commas thatwise it was treated as string.
+
+#question 29
+
+product_name = "telephone"
+product_quantity = 80
+product_price = 999.9
+product_available = True
+discount_information = None
+print(type(product_name),type(product_quantity),type(product_price),type(product_available),type(discount_information))
+
+#question 30
+
+value_integer=10
+value_decimal=10.0
+value_integer_commas="10"
+value_right=True
+value_right_commas="True"
+value_empty=None
+value_empty_commas="None"
+print(type(value_integer),type(value_decimal),type(value_integer_commas),type(value_right),type(value_right_commas),type(value_empty),type(value_empty_commas))
