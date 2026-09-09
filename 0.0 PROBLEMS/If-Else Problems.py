e1af53a51540248f8375915ee81f8d6dc2db4023
naman-soni09 <naman.soni.cg@gmail.com>
2026-09-09 09:34:58 +0530

    problems added

---
+++ 0.0 PROBLEMS/If-Else Problems.py
@@ -0,0 +1,109 @@
+# num=int(input("Enter Your Number-   "))
+# if num > 0:
+#     print("positive")
+# elif num == 0:
+#     print("zero")    
+# else:
+#     print("negative")
+
+
+# num=int(input("Enter Your Number-   "))
+# if num == 0:
+#     print("Zero")
+# elif num > 0 and num % 2 == 0:
+#     print("Positive Even")  
+# elif num > 0 and num % 2 != 0:
+#     print("Positive Odd")
+# elif num < 0 and num % 2 == 0:
+#     print("Negative Even")    
+# else:
+#     print("Negative Odd")    
+
+
+# num1=int(input("Enter First Number-  "))
+# num2=int(input("Enter Second Number-  "))
+# if num1 > num2:
+#     print(num1)
+# elif num2 > num1:
+#     print(num2)    
+# else:
+#     print("Both are equal")
+
+
+# num1=int(input("Enter First Number-  "))
+# num2=int(input("Enter Second Number-  "))
+# num3=int(input("Enter Third Number-   "))
+# if num1 <= num2 and num1 <= num3:
+#     print(num1)
+# elif num2 <= num1 and num2 <= num3:
+#     print(num2)
+# else:
+#     print(num3) 
+
+
+# num1=int(input("Enter First Number-  "))
+# num2=int(input("Enter Second Number-  "))
+# num3=int(input("Enter Third Number-   "))
+# if num1 >= num2 and num1 >= num3:
+#     print(str(num1) + " is the largest")
+# elif num2 >= num1 and num2 >= num3:
+#     print(str(num2) + " is the largest")
+# else:
+#     print(str(num3) + " is the largest") 
+          
+
+# num = int(input("Enter a number:  "))
+# if num % 5 == 0 and num % 11 == 0:
+#     print("Divisible by both 5 & 11")
+# elif num % 5 == 0:
+#     print("Divisible Only By 5")
+# elif num % 11 ==0:
+#     print("Divisible only by 11")
+# else:
+#     print("Divisible by Neither")
+
+
+# num = int(input("Enter a number: "))
+# if num % 3 == 0 and num % 7 == 0:
+#     print("Divisible by both 3 and 7")
+# elif num % 3 == 0:
+#     print("Divisible only by 3")
+# elif num % 7 == 0:
+#     print("Divisible only by 7")
+# else:
+#     print("Divisible by neither")
+
+
+# marks = int(input("Enter Marks:  "))
+# if marks < 0 or marks > 100:
+#     print("Invalid marks")
+# elif marks >= 40:
+#     print("Pass")
+# else:
+#     print("Fail")
+
+    
+# marks = float(input("Enter marks:  "))
+# if marks < 0 or marks > 100:
+#     print("Invalid marks")
+# elif marks >= 90:
+#     print("A")            
+# elif marks >= 80:
+#     print("B")
+# elif marks >= 70:
+#     print("C")
+# elif marks >= 60:
+#     print("D")
+# elif marks >= 40:
+#     print("E")
+# else:
+#     print("Fail")
+
+
+age = int(input("Enter age: "))
+if age < 0 or age > 120:
+    print("Invalid age")
+elif age >= 18:
+    print("Can vote")
+else:
+    print("Cannot vote")
